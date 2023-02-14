a2ccee1f90b94e69d54fd1522bea02f3d52aa6e5
Vadym Oliinyk <vadym.oliinyk@amomedia.com>
2023-02-14 20:52:51 +0200

    Not ideal, but it works
    Not ideal but works

--- src/useWebSocket.js
+++ src/useWebSocket.js
@@ -1,17 +1,19 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export function useWebSocket({ url }) {
   const [socket, setSocket] = useState();
 
-  function handleClose() {
+  const handleClose = useCallback(() => {
     socket?.close();
-  }
+  }, [socket]);
 
   useEffect(() => {
     setSocket(new WebSocket(url));
-
-    return handleClose;
   }, [url]);
+
+  useEffect(() => {
+    return handleClose;
+  }, [handleClose]);
 
   return socket;
 }
